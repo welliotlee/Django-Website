94d36d2077a98e3583e2e2956c22643a3a1bfc5b
William Lee <w.elliot.lee@gmail.com>
2017-11-09 23:15:23 -0800

    Categories now list

--- new_prog/newp_app1/admin.py
+++ new_prog/newp_app1/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from newp_app1.models import Category, Page
 
 # Register your models here.
-admin.site.register(Category)
 
 class PageAdmin(admin.ModelAdmin):
 	fields = ('category', 'title', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
